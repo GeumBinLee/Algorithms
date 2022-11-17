1b7dba2b877ca1c0cfb4a98a10d27f741b9ada86
GeumBin <lgb9811@gmail.com>
2022-11-17 16:41:36 +0900

    실패율 문제

--- second/실패율.py
+++ second/실패율.py
@@ -1,30 +1,23 @@
-from collections import Counter
-
 def solution(N, stages):
+    answer = []
+    
     # 스테이지에 도달한 플레이어의 수 구하기
     reach = [0 for _ in range(N)]
-    for stage in stages :
-        if stage > N :
-            for k in range(len(reach)) :
+    for stage in stages:
+        if stage > N:
+            for k in range(len(reach)):
                 reach[k] += 1
-        else : 
-            for n in range(stage) :
+        else:
+            for n in range(stage):
                 reach[n] += 1
     print(f"reach = {reach}")
-    
+
     # 클리어하지 못한 사람의 수 구하기
-    not_clear = dict(Counter(stages))
-    print(not_clear)
-    not_clear_list = [0 for _ in range(N)]
-    print(not_clear_list)
-    print(not_clear.keys())
-    for i in range(N) :
-        if not_clear[i+1]:
-            not_clear_list[i] = not_clear[i+1]
-        else :
-            not_clear_list[i] = 0
-    print(not_clear_list)
-    
+    not_clear = [0 for _ in range(N)]
+    for i in range(1, N+1):
+        not_clear[i-1] = stages.count(i)
+    print(f"클리어 ㄴㄴ ={not_clear}")
+
     # 실패율 구하기 : 아직 클리어하지 못한 사람 수 / 스테이지에 도달한 플레이어 수
     fail = {}
     for a in range(N) :
@@ -34,9 +27,12 @@ def solution(N, stages):
             per = not_clear[a]/reach[a]
             fail[eval(f"{a}+1")] = per
             
-    answer = dict(sorted(fail.items()))
-    print(answer)
-    
+    before_answer = sorted(fail.items(), reverse=True, key=lambda x : x[1])
+    for an in before_answer :
+        answer.append(an[0])
     return answer
+        
+        
+    
 
 solution(5, [2, 1, 2, 6, 2, 4, 3, 3])
