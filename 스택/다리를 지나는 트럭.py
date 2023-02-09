f9fc8b6586fe71ec99091f8bfa983feeb3f44f9e
GeumBin <lgb9811@gmail.com>
2023-02-09 22:46:40 +0900

    다리를 지나는 트럭 통과 코드

--- 스택/다리를 지나는 트럭.py
+++ 스택/다리를 지나는 트럭.py
@@ -32,4 +32,20 @@ from collections import deque
 #     answer = sum(truck_weights) + bridge_length
 #     return answer
 
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    truck_bridge_deque = deque(bridge_length * [0])
+    truck_weights_deque = deque(truck_weights)
+    while len(truck_bridge_deque):
+        answer += 1
+        truck_bridge_deque.popleft()
+        if truck_weights_deque:
+            if sum(truck_bridge_deque) + truck_weights_deque[0] <= weight:
+                truck_bridge_deque.append(truck_weights_deque.popleft())
+            else:
+                truck_bridge_deque.append(0)
+    return answer
+
 solution(100, 100, [10,10,10,10,10,10,10,10,10,10])
